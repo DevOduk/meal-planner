53ed924bca84d2f007bc6b771233c7accfdea537
DevOduk <austine.oduk577@gmail.com>
2026-06-07 21:04:18 +0300

    May update algorithm update and ui fix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Footer() {
     return (
         <div className="mb-6 text-center">
-            @Copyright Meal Planner 2026 Designed and developed by DevOduk
+            @Copyright Meal Planner 2026 Designed and developed by <a href='https://github.com/DevOduk' target='_blank' rel='norefeerer'>DevOduk</a> 
             <br />
             <br />
             <br />
